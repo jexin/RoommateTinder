469cfae651da1c8614330b2668e9d82e6b0a287e
Jenny Xin <jennyxin28@gmail.com>
2018-07-31 16:57:09 -0700

    edit profile

--- javascript/profile.js
+++ javascript/profile.js
@@ -23,3 +23,11 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+// SelectElement("gender", "Other")
+//
+// function SelectElement(id, valueToSelect)
+// {
+//     var element = document.getElementById(id);
+//     element.value = valueToSelect;
+// }
