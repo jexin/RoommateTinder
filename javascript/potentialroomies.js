98609b81df89520f522150f78a97825ba322fb86
Lily Ou <ou.lily890@gmail.com>
2018-08-01 16:50:38 -0700

    filter bar checking works

--- javascript/potentialroomies.js
+++ javascript/potentialroomies.js
@@ -1,43 +1,43 @@
 $('#city_filter').click(function(e){
 	if (e.target.checked) {
-  	localStorage.checked = "checked"; //string is true
+  	localStorage.checked_city = "checked"; //string is true
   } else {
-  	localStorage.checked = ""; //empty string is false
+  	localStorage.checked_city = ""; //empty string is false
   }
 })
 $( document ).ready(function() {
-	document.querySelector('#city_filter').checked = localStorage.checked;
+	document.querySelector('#city_filter').checked = localStorage.checked_city;
 });
 
 $('#state_filter').click(function(e){
 	if (e.target.checked) {
-  	localStorage.checked = "checked";
+  	localStorage.checked_state = "checked";
   } else {
-  	localStorage.checked = "";
+  	localStorage.checked_state = "";
   }
 })
 $( document ).ready(function() {
-	document.querySelector('#state_filter').checked = localStorage.checked;
+	document.querySelector('#state_filter').checked = localStorage.checked_state;
 });
 
 $('#smoking_filter').click(function(e){
 	if (e.target.checked) {
-  	localStorage.checked = "checked";
+  	localStorage.checked_smoking = "checked";
   } else {
-  	localStorage.checked = "";
+  	localStorage.checked_smoking = "";
   }
 })
 $( document ).ready(function() {
-	document.querySelector('#smoking_filter').checked = localStorage.checked;
+	document.querySelector('#smoking_filter').checked = localStorage.checked_smoking;
 });
 
 $('#hobbies_filter').click(function(e){
 	if (e.target.checked) {
-  	localStorage.checked = "checked";
+  	localStorage.checked_hobbies = "checked";
   } else {
-  	localStorage.checked = "";
+  	localStorage.checked_hobbies = "";
   }
 })
 $( document ).ready(function() {
-	document.querySelector('#hobbies_filter').checked = localStorage.checked;
+	document.querySelector('#hobbies_filter').checked = localStorage.checked_hobbies;
 });
