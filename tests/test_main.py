7a116b2c8ea5fec16611feca164958431388f7e7
Julia Cordero <juliaraquel200@gmail.com>
2018-08-01 21:21:41 -0700

    style changes and minor bugs

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-# #rename _main.py to reflect the function name, not the file name
-# import unittest
-# from main.py import #import the fuction
-# #import all things that you imported to make main.py work, like jinja and os
-#
-# class TestFunction(unittest.TestCase): #rename test function to be descriptive of what we're testing
-#     def test_function(self): #rename test_function to be descriptive
-#         self.assertAlmostEqual() #change to a different assert method, for whatever the function returns
-#         #add more testcases
